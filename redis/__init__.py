50b3acfc7eadf2ab70122935b4b4255362991843
learner1992 <makebelieving@163.com>
2018-09-06 22:36:52 +0800

    test

---
+++ redis/__init__.py
@@ -0,0 +1,7 @@
+#-*- encoding: utf-8 -*-
+"""
+__init__.py.py
+Created on 2018/9/6 22:34
+Copyright (c) 2018/9/6.
+@author: ogc
+"""
---
+++ redis/lock/DistributeLock.py
@@ -0,0 +1,8 @@
+#-*- encoding: utf-8 -*-
+"""
+DistributeLock.py
+Created on 2018/9/6 22:35
+Copyright (c) 2018/9/6.
+@author: ogc
+"""
+print "hello"
